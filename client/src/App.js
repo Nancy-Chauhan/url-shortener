c598404210e7c72c8e5f4a7e35b4dd74dbd16a31
Nancy Chauhan <nancychn1@gmail.com>
2020-04-05 19:11:23 +0530

    Modify UI

--- client/src/App.js
+++ client/src/App.js
@@ -26,22 +26,25 @@ class App extends React.Component {
     event.preventDefault();
   }
 
+  copyUrlToClipboard = () => {
+    navigator.clipboard.writeText(`http://localhost:8080/redirect/${this.state.shortenedUrl}`)
+  }
+
   render() {
     const { shortenedUrl, url } = this.state;
     return (
       <div className="App">
         <main>
           <form onSubmit={this.handleSubmit}>
-            <label>
-              Name:
-              <input type="text" value={url} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
+            <div class="wrapper">
+              <input className="urlbox" id="url" type="url" placeholder="Paste Url here" value={url} onChange={this.handleChange} />
+              <input class="submit" type="submit" value="Submit" />
+            </div>
           </form>
-          <p>
-
-            {shortenedUrl && <a href={`http://localhost:8080/redirect/${shortenedUrl}`}>http://localhost:8080/redirect/{shortenedUrl}</a>}
-          </p>
+          {shortenedUrl && (<p className="shortenedurl">
+            <a href={`http://localhost:8080/redirect/${shortenedUrl}`} target="_blank">http://localhost:8080/redirect/{shortenedUrl}</a>
+            <button onClick={this.copyUrlToClipboard} class="copy" type="button">Copy</button>  
+          </p>)}
         </main>
       </div>
     );
